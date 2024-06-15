b746c236f9a2758c7e7f05af29cc85ecb621e013
dinesh061 <115059054+dinesh061@users.noreply.github.com>
2024-06-15 10:58:40 +0530

    Update HomeScreen.js

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -45,7 +45,7 @@ function HomeScreen() {
   return (
     <div>
       <Helmet>
-        <title>E commerce  </title>
+        <title>Dinesh_app</title>
       </Helmet>
       <h1>Featured Products</h1>
       <div className="products">
